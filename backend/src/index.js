4d64037d7a3d50b86ec1e1dcc3d9d3e797a27146
Lucas Cavalheri <lucas.dev.carvalho@gmail.com>
2025-05-31 13:26:05 -0300

    backend: message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const authRoutes = require('./routes/auth.routes')
+const messageRoutes = require('./routes/message.routes')
 const { connectDB } = require('./lib/db')
 require('dotenv').config()
 
@@ -10,6 +11,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
+app.use('/api/messages', messageRoutes)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`)
