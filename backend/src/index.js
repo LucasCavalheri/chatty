f2b5e89875605560ee2a76e04eb201a635a535b0
Lucas Cavalheri <lucas.dev.carvalho@gmail.com>
2025-05-31 14:15:25 -0300

    backend: cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const authRoutes = require('./routes/auth.routes')
 const messageRoutes = require('./routes/message.routes')
@@ -9,6 +10,10 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
 
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
