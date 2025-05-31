9734a4ca5420b92920516d0c3bf8f8d8591d9fd6
Lucas Cavalheri <lucas.dev.carvalho@gmail.com>
2025-05-31 20:53:37 -0300

    frontend: settings page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,13 @@ import { SettingsPage } from './pages/SettingsPage'
 import { ProfilePage } from './pages/ProfilePage'
 import { Loader } from 'lucide-react'
 import { useAuthStore } from './store/useAuthStore'
+import { useThemeStore } from './store/useThemeStore'
 import { useEffect } from 'react'
 import { Toaster } from 'react-hot-toast'
 
 export const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
+  const { theme } = useThemeStore()
 
   useEffect(() => {
     checkAuth()
@@ -26,7 +28,7 @@ export const App = () => {
   }
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
