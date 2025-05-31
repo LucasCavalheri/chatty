252f5c99a12dc04dc2c5f103d9e2115aa6b7b748
Lucas Cavalheri <lucas.dev.carvalho@gmail.com>
2025-05-31 20:40:38 -0300

    frontend: signup, login and logout pages

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,61 @@
+import { create } from 'zustand'
+import { api } from '../lib/axios'
+import toast from 'react-hot-toast'
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const response = await api.get('/auth/check')
+
+      set({ authUser: response.data })
+    } catch (error) {
+      console.log(error)
+      set({ authUser: null })
+    } finally {
+      set({ isCheckingAuth: false })
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true })
+    try {
+      const res = await api.post('/auth/signup', data)
+      set({ authUser: res.data })
+      toast.success('Conta criada com sucesso!')
+    } catch (error) {
+      toast.error(error.response.data.message)
+    } finally {
+      set({ isSigningUp: false })
+    }
+  },
+
+  login: async (data) => {
+    set({ isLoggingIn: true })
+    try {
+      const res = await api.post('/auth/login', data)
+      set({ authUser: res.data })
+      toast.success('Logado com sucesso!')
+    } catch (error) {
+      toast.error(error.response.data.message)
+    } finally {
+      set({ isLoggingIn: false })
+    }
+  },
+
+  logout: async () => {
+    try {
+      await api.post('/auth/logout')
+      set({ authUser: null })
+      toast.success('Deslogado com sucesso!')
+    } catch (error) {
+      console.log(error)
+      toast.error('Ocorreu um erro ao deslogar')
+    }
+  }
+}))
